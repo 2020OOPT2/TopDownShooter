7faeef5488924e021b87bdacab530ff1cb107c3f
정민규 <fastdodge7@naver.com>
2020-07-22 16:11:51 +0900

    테스트 겸 이동 스크립트 추가
    테스트 겸으로 큐브를 움직일 수 있는 스크립트를 추가해 봤습니다.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Move_Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+            this.transform.position += new Vector3(0, 1, 0) * Move_Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            this.transform.position += new Vector3(0, -1, 0) * Move_Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            this.transform.position += new Vector3(-1, 0, 0) * Move_Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            this.transform.position += new Vector3(1, 0, 0) * Move_Speed * Time.deltaTime;
+    }
+}
