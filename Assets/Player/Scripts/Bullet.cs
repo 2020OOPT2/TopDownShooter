8fdbb5e9bf238f4d34e8869fc8a7b43544b93e34
정민규 <fastdodge7@naver.com>
2020-07-24 22:00:50 +0900

    총알이 화살과 충돌 시 사라지는 버그 수정
    화살의 Tag를 Enemy_Bullet으로 설정하고 Bullet 스크립트에 Destroy 조건을 추가하였습니다.

--- Assets/Player/Scripts/Bullet.cs
+++ Assets/Player/Scripts/Bullet.cs
@@ -18,9 +18,10 @@ public class Bullet : MonoBehaviour
         if (this.transform.position.x > 20 || this.transform.position.x < -20 || this.transform.position.y > 20 || this.transform.position.y < -20)
             Destroy(this.gameObject); // 총알이 일정 범위를 넘어서면 해당 총알을 지웁니다.
     }
+  
     private void OnTriggerEnter2D(Collider2D collision) // 총알끼리 충돌하는 것을 막기 위해 Is trigger를 체크해 둔 상태입니다.
     {
-        if (collision.gameObject.tag != "Player") // 이후 벽 등이 추가될 경우 조건이 추가되어야 할 수 있습니다.
+        if (collision.gameObject.tag != "Player" && collision.gameObject.tag != "Enemy_Bullet") // 이후 벽 등이 추가될 경우 조건이 추가되어야 할 수 있습니다.
         {
             Debug.Log("적을 맞혔습니다!");
             Destroy(this.gameObject);
