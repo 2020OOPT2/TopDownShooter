5668ed1d63831e8ab8144748aa55f75363d33abc
hosung1999 <68551720+hosung1999@users.noreply.github.com>
2020-07-31 20:33:53 +0900

    UI

--- Assets/UI/Scripts/Game_Over.cs
+++ Assets/UI/Scripts/Game_Over.cs
@@ -7,19 +7,21 @@ public class Game_Over : MonoBehaviour
     float AfterDeathTime = 0;
     public GameObject IngameScreen;
     public GameObject GameOver;
+
     void Update()
     {
         float CurHP = GameObject.Find("Player").GetComponent<Player>().Player_Current_HP;
-        if(CurHP <= 0)
+        if (CurHP <= 0)
         {
-            AfterDeathTime = AfterDeathTime + Time.deltaTime;
+            AfterDeathTime = AfterDeathTime + Time.deltaTime;           
+            GameObject.Find("Player").GetComponent<Player>().Player_Audio.Play();
             Debug.Log("AfterDeathTime" + AfterDeathTime);
-            if(AfterDeathTime >= 2)
+            if (AfterDeathTime >= 2)
             {
                 IngameScreen.SetActive(false);
                 GameOver.SetActive(true);
             }
-            
+
         }
     }
 }
