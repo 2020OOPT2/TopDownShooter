e327e1a422653125c87b6272d7e977d8b0559649
hosung1999 <68551720+hosung1999@users.noreply.github.com>
2020-08-07 22:33:01 +0900

    BulletTime바 추가, UI 개선

--- Assets/UI/Scripts/Bar.cs
+++ Assets/UI/Scripts/Bar.cs
@@ -7,6 +7,7 @@ public class Bar : MonoBehaviour
 {
     public Slider HPBar;
     public Slider StaminaBar;
+    public Slider BulletTimeBar;
     
 
     void Start()
@@ -15,9 +16,11 @@ public class Bar : MonoBehaviour
         float MaxHP = GameObject.Find("Player").GetComponent<Player>().Player_Max_HP;
         float CurStamina = GameObject.Find("Player").GetComponent<Player>().Player_Current_Stamina;
         float MaxStamina = GameObject.Find("Player").GetComponent<Player>().Player_Max_Stamina;
-
+        float MaxTimeForce = GameObject.Find("Player").GetComponent<Player_Time_Control>().Max_Time_Force;
+        float CurTimeforce = GameObject.Find("Player").GetComponent<Player_Time_Control>().Cur_Time_Force;
         HPBar.value = CurHP / MaxHP;
         StaminaBar.value = CurStamina / MaxStamina;
+        BulletTimeBar.value = CurTimeforce / MaxTimeForce;
     }
 
     void Update()
@@ -26,8 +29,11 @@ public class Bar : MonoBehaviour
         float MaxHP = GameObject.Find("Player").GetComponent<Player>().Player_Max_HP;
         float CurStamina = GameObject.Find("Player").GetComponent<Player>().Player_Current_Stamina;
         float MaxStamina = GameObject.Find("Player").GetComponent<Player>().Player_Max_Stamina;
+        float MaxTimeForce = GameObject.Find("Player").GetComponent<Player_Time_Control>().Max_Time_Force;
+        float CurTimeforce = GameObject.Find("Player").GetComponent<Player_Time_Control>().Cur_Time_Force;
         HPBar.value = CurHP / MaxHP;
         StaminaBar.value = CurStamina / MaxStamina;
+        BulletTimeBar.value = CurTimeforce / MaxTimeForce;
         //Debug.Log(HPBar.value);
         //Debug.Log(StaminaBar.value);
     }
