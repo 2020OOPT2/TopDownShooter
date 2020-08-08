df2e2a8f34996d6aabe201ab562c9a4d628971a4
JK3WN <yimjay2@naver.com>
2020-08-08 21:22:49 +0900

    무한스폰(일단 완성)

--- Assets/Scripts/Spawning.cs
+++ Assets/Scripts/Spawning.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Spawning : MonoBehaviour
 {
-    private int side, pos, type;
-    private float time;
+    private int side, type;
+    private float time, pos;
     public float spawntime;
+    public GameObject Zom, Ske, Gho;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,7 @@ public class Spawning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time = Time.deltaTime;
+        time += Time.deltaTime;
         if (time >= spawntime)
         {
             side = Random.Range(0, 4);
@@ -24,12 +25,37 @@ public class Spawning : MonoBehaviour
             if (side % 2 == 0)
             {
                 pos = Random.Range(-19, 20);
+                if (side == 0)
+                {
+                    if (type == 0) Instantiate(Zom, new Vector3(575 + pos, 264 + 15, 0), Quaternion.identity);
+                    else if (type == 1) Instantiate(Ske, new Vector3(575 + pos, 264 + 15, 0), Quaternion.identity);
+                    else Instantiate(Gho, new Vector3(575 + pos, 264 + 15, 0), Quaternion.identity);
+                }
+                else
+                {
+                    if (type == 0) Instantiate(Zom, new Vector3(575 + pos, 264 - 15, 0), Quaternion.identity);
+                    else if (type == 1) Instantiate(Ske, new Vector3(575 + pos, 264 - 15, 0), Quaternion.identity);
+                    else Instantiate(Gho, new Vector3(575 + pos, 264 - 15, 0), Quaternion.identity);
+                }
             }
             else
             {
                 pos = Random.Range(-15, 16);
+                if (side == 1)
+                {
+                    if (type == 0) Instantiate(Zom, new Vector3(575 + 19, 264 + pos, 0), Quaternion.identity);
+                    else if (type == 1) Instantiate(Ske, new Vector3(575 + 19, 264 + pos, 0), Quaternion.identity);
+                    else Instantiate(Gho, new Vector3(575 + 19, 264 + pos, 0), Quaternion.identity);
+                }
+                else
+                {
+                    if (type == 0) Instantiate(Zom, new Vector3(575 - 19, 264 + pos, 0), Quaternion.identity);
+                    else if (type == 1) Instantiate(Ske, new Vector3(575 - 19, 264 + pos, 0), Quaternion.identity);
+                    else Instantiate(Gho, new Vector3(575 - 19, 264 + pos, 0), Quaternion.identity);
+                }
             }
             time = 0;
+            Debug.Log("pos:" + pos);
         }
     }
 }
